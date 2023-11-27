71d70b8bd538c2603bc1322f53909dcc5ab1ac73
Antoine Orange <antoine.orange@epitech.eu>
2023-11-27 12:05:08 +0100

    [-] .idea

---
+++ rezise.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** scene_check.c
+** File description:
+** this file contains all the
+** necessary functions for the
+** check of scene.
+** created by Antoine Orange
+** All rights reserved
+*/
+
+#include "my.h"
+#include "list.h"
+
+void resize() {
